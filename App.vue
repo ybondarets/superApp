95fe9a4bf716e1d25716a0647ff4b6b2bb467c9c
boslaw <yaroslawbonarets@gmail.com>
2019-03-28 09:23:07 +0200

    Initial commit

---
+++ App.vue
@@ -0,0 +1,15 @@
+<template>
+    <main></main>
+</template>
+
+<script>
+    import Main from './src/main/Main';
+
+    export default {
+        components: {
+            Main
+        }
+    }
+</script>
+ 
+<style></style>
---
+++ src/ui/WeatherView/WeatherView.vue
@@ -0,0 +1,15 @@
+<template>
+    <text>
+        Weather view
+    </text>
+</template>
+
+<script>
+    export default {
+        name: "Weather"
+    }
+</script>
+
+<style scoped>
+
+</style>
